7244774016f24744a0a4d5eb0a67f55d5732daf0
Marcos Vieira <marcoshv1964@gmail.com>
2017-07-12 00:46:48 -0300

    continuacao da aula 22

--- main.js
+++ main.js
@@ -26,7 +26,53 @@ Vue.component('mensagem', {
                 <button class="delete" @click="escondeMensagem()"></button>
             </div>
             <div class="message-body">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+                <slot></slot>
+            </div>
+        </article>`,
+    data: function() {
+        return {
+            visivel: true
+        };
+    },
+    methods: {
+        escondeMensagem: function() {
+            this.visivel = false;
+        }
+    }
+});
+
+Vue.component('advertencia', {
+    template: `
+        <article class="message is-warning" v-show="visivel">
+            <div class="message-header">
+                <p>Olá Mundo pelo Bulma !!!</p>
+                <button class="delete" @click="escondeMensagem()"></button>
+            </div>
+            <div class="message-body">
+                <slot></slot>
+            </div>
+        </article>`,
+    data: function() {
+        return {
+            visivel: true
+        };
+    },
+    methods: {
+        escondeMensagem: function() {
+            this.visivel = false;
+        }
+    }
+});
+
+Vue.component('erro', {
+    template: `
+        <article class="message is-danger" v-show="visivel">
+            <div class="message-header">
+                <p>Olá Mundo pelo Bulma !!!</p>
+                <button class="delete" @click="escondeMensagem()"></button>
+            </div>
+            <div class="message-body">
+                <slot></slot>
             </div>
         </article>`,
     data: function() {
